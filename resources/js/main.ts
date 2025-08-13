2a9e99a7980e908db64f970961ee5ba9e53286c2
WaWaEMu <wawa2y111@gmail.com>
2025-08-13 12:17:31 +0800

    fix: configure Sanctum stateful domains and axios credentials to resolve 401 errors

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import router from '@/router'
 import './permission';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000';
 
 const app = createApp(App);
 app.use(router);
