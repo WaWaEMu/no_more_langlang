eda2b8413c4b6a05efbc6e77c93ec9a1643572ae
WaWaEMu <wawa2y111@gmail.com>
2025-09-04 19:26:25 +0800

    feat: install bootstrap-icons package

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -2,6 +2,7 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
 import router from '@/router'
 import './permission';
