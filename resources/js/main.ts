bd045a6b0907664062ca30c507a0720fe02a8226
WaWaEMu <wawa2y111@gmail.com>
2025-11-15 09:20:32 +0800

    chore: install pinia and set up store

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -7,11 +7,15 @@ import 'bootstrap';
 import router from '@/router'
 import './permission';
 import axios from 'axios';
+import { createPinia } from 'pinia'
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:8000';
 
 const app = createApp(App);
+const pinia = createPinia();
+
 app.use(router);
+app.use(pinia);
 
 app.mount('#app');
