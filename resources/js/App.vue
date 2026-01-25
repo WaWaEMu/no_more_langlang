df92a51efd78ef4aa0697db6f2ef35cd392f1982
WaWaEMu <wawa2y111@gmail.com>
2026-01-26 00:58:26 +0800

    feat(auth): force external browser for LINE to fix Google login

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+    const ua = navigator.userAgent;
+    const isLine = /Line/i.test(ua);
+    const url = new URL(window.location.href);
+
+    // If in LINE and the external browser parameter is not yet present
+    if (isLine && !url.searchParams.has('openExternalBrowser')) {
+        url.searchParams.set('openExternalBrowser', '1');
+        window.location.replace(url.toString());
+    }
+});
+</script>
+
 <template>
     <RouterView></RouterView>
 </template>
